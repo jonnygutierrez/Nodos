20f2cf1e3807c50ab2e77e360efc850c386050e9
realcalcaneosantiago <realcalcaneosantiago@gmail.com>
2025-02-18 22:11:57 +0000

    chnages responsive slider

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,7 +4,7 @@ import { SkillsComponent } from './skills/skills.component';
 import { UsServicesComponent } from './us-services/us-services.component';
 import { BriefcaseComponent } from './briefcase/briefcase.component';
 import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
-
+import { CardInfoComponent } from './components/card-info/card-info.component';
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -13,11 +13,22 @@ import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
     SkillsComponent,
     UsServicesComponent,
     BriefcaseComponent,
-    NavBarComponent, NavBarComponent
+    NavBarComponent, 
+    CardInfoComponent
 ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  items: string[] = ['Slide 1', 'Slide 2', 'Slide 3', 'Slide 4']; // Datos de ejemplo
 
+  ngAfterViewInit(): void {
+    // Inicializa Swiper después de que la vista se haya cargado
+    new (window as any).Swiper('.swiper', {
+ 
+      autoplay:{
+        delay:5000
+      }
+    });
+  }
 }
