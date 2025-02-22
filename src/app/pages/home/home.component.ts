ea362d8832d21935fa8c24b7abafb093686a105d
RealSantiago <realcalcaneosantiago@gmail.com>
2025-02-21 22:47:01 -0600

    chnages responseive skills

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,7 +3,7 @@ import { HeaderComponent } from './header/header.component';
 import { SkillsComponent } from './skills/skills.component';
 import { UsServicesComponent } from './us-services/us-services.component';
 import { BriefcaseComponent } from './briefcase/briefcase.component';
-import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { CardInfoComponent } from './components/card-info/card-info.component';
 @Component({
   selector: 'app-home',
@@ -13,33 +13,80 @@ import { CardInfoComponent } from './components/card-info/card-info.component';
     SkillsComponent,
     UsServicesComponent,
     BriefcaseComponent,
-    NavBarComponent, 
-    CardInfoComponent
-],
+    NavBarComponent,
+    CardInfoComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  items: string[] = ['Slide 1', 'Slide 2', 'Slide 3', 'Slide 4','','','','']; // Datos de ejemplo
+  skills = [
+    {
+      id: 1,
+      name: 'Angular',
+      icon: 'angular',
+      src: '../../../assets/img/skills/angular_logo.png',
+    },
+    {
+      id: 2,
+      name: 'HTML',
+      icon: 'angular',
+      src: '../../../assets/img/skills/html_logo.png',
+    },
+    {
+      id: 3,
+      name: 'Laravel',
+      icon: 'angular',
+      src: '../../../assets/img/skills/laravel.png',
+    },
+    {
+      id: 4,
+      name: 'Google Maps',
+      icon: 'angular',
+      src: '../../../assets/img/skills/google_maps.png',
+    },
+    {
+      id: 5,
+      name: 'IONIC',
+      icon: 'angular',
+      src: '../../../assets/img/skills/ionic.png',
+    },
+    {
+      id: 6,
+      name: 'MySQL',
+      icon: 'angular',
+      src: '../../../assets/img/skills/mysql.png',
+    },
+    {
+      id: 7,
+      name: 'Ubuntu',
+      icon: 'angular',
+      src: '../../../assets/img/skills/ubuntu_logo.png',
+    },
+  ];
 
   ngAfterViewInit(): void {
     // Inicializa Swiper después de que la vista se haya cargado
     new (window as any).Swiper('.swiper', {
-      slidesPerView: 3,
+      slidesPerView: 2,
       spaceBetween: 0,
-      loop:true,
-      autoplay:{
-        delay:5000
+
+      loop: true,
+      autoplay: {
+        delay: 5000,
       },
 
       breakpoints: {
+        640: {
+          slidesPerView: 3,
+        },
         768: {
-          slidesPerView: 3.5,
-          spaceBetween: 30,
+          slidesPerView: 4,
+        },
+        1440: {
+          slidesPerView: 7,
         },
       },
-
-      
     });
   }
 }
