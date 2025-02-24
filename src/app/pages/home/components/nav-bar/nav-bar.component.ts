0fddb8bb0e1d2e2fddfecd67bf2a74b2add8e45d
RealSantiago <realcalcaneosantiago@gmail.com>
2025-02-24 00:44:39 -0600

    changes responsive and ready navBar component

--- src/app/pages/home/components/nav-bar/nav-bar.component.ts
+++ src/app/pages/home/components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss'
+  styleUrl: './nav-bar.component.scss',
 })
-export class NavBarComponent {
+export class NavBarComponent implements AfterViewInit {
+  @ViewChild('menu') menu?: ElementRef;
+  @ViewChild('lists') listm?: ElementRef;
+  isMenuOpen: boolean = false;
 
+  ngAfterViewInit(): void {
+    console.log(this.menu);
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
